d7dd15067d8b5bb4bdfed21daab52959c5b082c4
efraimdahl <efraimdahl@uchicago.edu>
2023-05-04 02:17:32 -0500

    added routing precalculations and display

--- js/app.js
+++ js/app.js
@@ -16,11 +16,62 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 var marker = L.marker([-6.175247, 106.8270488],{icon: mapMarker}).addTo(map).bindPopup("<b>TESTTESTTEST</b>")
+let prev = null
+let travelDistances = {"Flight":0,"Train":0,"Bus":0,"Taxi":0,"Bike":0,"Other":0,"Total":0}
 data.forEach(item => {
+    if(item["Latitude"]!=0 & item["Latitude"]!=0){
     console.log(item["Name"],item["Longitude"],item["Latitude"])
     var marker = L.marker([item["Longitude"], item["Latitude"]],{icon: mapMarker}).addTo(map)
-
+    if(prev!=null){
+        let backup = true
+        let color = 'black'
+        switch(prev["Method"]){
+            case "Bike":
+                color = 'green'
+                break;
+            case "Taxi":
+                color = 'yellow'
+                break;
+            case "Train":
+                color = 'red'
+                break;
+            case "Bus":
+                color = 'blue'
+                break;}
+        if(prev["Method"] !="Flight" && item["Route"]!=0 &&item["Route"]!={}){
+            //console.log(item["Route"])
+            let pathjson=JSON.parse(item["Route"])
+            //console.log(pathjson)
+            if(Object.keys(pathjson).length != 0){
+                backup=false
+                let latlngs = pathjson["route"]["geometry"]["coordinates"];
+                let distance = pathjson["route"]["distance"]
+                travelDistances[prev["Method"]]+=distance
+                travelDistances["Total"]+=distance
+                let polyline = L.polyline(latlngs, {color: color}).addTo(map);
+            }
+        }
+        if(backup){
+            let pointA = new L.LatLng(prev["Point"][0], prev["Point"][1]);
+            let pointB = new L.LatLng(item["Longitude"], item["Latitude"]);
+            let distance = pointA.distanceTo(pointB)
+            travelDistances[prev["Method"]]+=distance
+                travelDistances["Total"]+=distance
+            let pointList = [pointB,pointA]
+            let firstpolyline = new L.Polyline(pointList, {
+                color: color,
+                weight: 3,
+                opacity: 0.5,
+                smoothFactor: 1
+            });
+            firstpolyline.addTo(map);
+        }
+    }
+    
+    prev={"Method":item["Method"],"Point":[item["Longitude"],item["Latitude"]]}
     marker.bindPopup("<b>"+item["Name"]+item["Longitude"]+"<br>"+item["Latitude"]+"</b>")
+}
 })
+console.log(travelDistances)
 
 
